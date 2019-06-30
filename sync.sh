2dd1693537d6778dbb1be78024575964c9b93ea9
Ellen Marie Dash <me@duckie.co>
2019-06-30 16:28:46 -0400

    Actually make sync.sh work.

--- sync.sh
+++ sync.sh
@@ -2,8 +2,16 @@
 
 echo "Syncing to $(echo /run/media/*/CIRCUITPY/)"
 
-while true; do
+function update() {
+  if [ -z "$(diff code.py /run/media/*/CIRCUITPY/code.py)" ]; then
+    return
+  fi
+
   cp code.py /run/media/*/CIRCUITPY/code.py
   sync -f /run/media/*/CIRCUITPY/code.py
+}
+
+while true; do
+  update
   sleep 5
 done
