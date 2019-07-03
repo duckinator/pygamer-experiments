3e6382c5957b9e35296d8a500129b94c763b67f1
Ellen Marie Dash <me@duckie.co>
2019-07-03 10:12:55 -0400

    Eh?

--- sync.sh
+++ sync.sh
@@ -4,8 +4,11 @@ echo "Syncing to $(echo /run/media/*/CIRCUITPY/)"
 
 function update() {
   if [ -z "$(diff code.py /run/media/*/CIRCUITPY/code.py)" ]; then
+    echo "No change."
     return
   fi
+
+  echo "Syncing code changes."
 
   cp code.py /run/media/*/CIRCUITPY/code.py
   sync -f /run/media/*/CIRCUITPY/code.py
